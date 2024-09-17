0ecb177a634930c24b52283b97feebe73289d9b4
CyberSinister <sinistercipher@yandex.com>
2024-09-17 10:31:00 +0500

    removed unnecessary things, changed tm

--- src/components/auth-footer/authFooter.vue
+++ src/components/auth-footer/authFooter.vue
@@ -2,18 +2,15 @@
     <div id="kt_app_footer" class="app-footer bg-body-tertiary " bis_skin_checked="1">
         <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3 " bis_skin_checked="1">
             <div class="text-gray-900 order-2 order-md-1" bis_skin_checked="1">
-              <span class="text-muted fw-semibold me-1">2024©</span>
-              <a href="https://silquetech.com/" target="_blank" class="text-gray-800 text-hover-primary">Cosys S.C</a>
+              <a href="https://flexup.app" target="_blank" class="text-gray-800 text-hover-primary">FlexUp </a>
+              <span class="text-muted fw-semibold me-1">© 2024</span>
             </div>
             <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
               <li class="menu-item">
-                <a href="https://silquetech.com/about" target="_blank" class="menu-link px-2">Legal</a>
+                <a href="/legal#termsofservice" target="_blank" class="menu-link px-2">Terms of Service</a>
               </li>
               <li class="menu-item">
-                <a href="https://silquetech.com/contact" target="_blank" class="menu-link px-2">SilqueTech</a>
-              </li>
-              <li class="menu-item">
-                <a href="https://silquetech.com/pricing" target="_blank" class="menu-link px-2">Support</a>
+                <a href="/legal#privacypolicy" target="_blank" class="menu-link px-2">Privacy Policy</a>
               </li>
             </ul>
         </div>
