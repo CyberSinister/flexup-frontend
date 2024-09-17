ada7c3fc43cbb6b1c1bb90a137cf15ecd18f3f13
CyberSinister <sinistercipher@yandex.com>
2024-09-17 10:31:38 +0500

    cleaned footer, added tm

--- src/layouts/default-layout/components/footer/Footer.vue
+++ src/layouts/default-layout/components/footer/Footer.vue
@@ -2,22 +2,18 @@
   <div id="kt_app_footer" class="app-footer " bis_skin_checked="1">
     <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3 " bis_skin_checked="1">
         
-        <div class="text-gray-900 order-2 order-md-1" bis_skin_checked="1">
-            <span class="text-muted fw-semibold me-1">2024©</span>
-            <a href="https://silquetech.com/" target="_blank" class="text-gray-800 text-hover-primary">Cosys S.C</a>
-        </div>
-
-        <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
-            <li class="menu-item">
-              <a href="https://silquetech.com/about" target="_blank" class="menu-link px-2">Legal</a>
-            </li>
-            <li class="menu-item">
-              <a href="https://silquetech.com/contact" target="_blank" class="menu-link px-2">SilqueTech</a>
-            </li>
-            <li class="menu-item">
-              <a href="https://silquetech.com/pricing" target="_blank" class="menu-link px-2">Support</a>
-            </li>
-        </ul>
+      <div class="text-gray-900 order-2 order-md-1" bis_skin_checked="1">
+        <a href="https://flexup.app" target="_blank" class="text-gray-800 text-hover-primary text-muted fw-semibold">FlexUp </a>
+        <span class="text-muted fw-semibold me-1">© 2024</span>
+      </div>
+      <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
+        <li class="menu-item">
+          <a href="/legal#termsofservice" target="_blank" class="menu-link px-2">Terms of Service</a>
+        </li>
+        <li class="menu-item">
+          <a href="/legal#privacypolicy" target="_blank" class="menu-link px-2">Privacy Policy</a>
+        </li>
+      </ul>
         
     </div>
   </div>
